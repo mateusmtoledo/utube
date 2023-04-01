12b8165e54c629e75a5277bbcb73aa91bf81a913
Mateus Toledo <mateusmtoledo1@gmail.com>
2023-04-01 14:02:49 -0300

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>UTube</title>
+        <meta name="description" content="UTube" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Welcome to UTube!</h1>
+      </main>
+    </>
+  );
+}
