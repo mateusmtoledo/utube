4592932fa42d168bb4fc2f1deb1d536fa090bdf4
Mateus Toledo <mateusmtoledo1@gmail.com>
2023-04-11 14:29:42 -0300

    Fix api URL in vercel

--- adapters/axios.ts
+++ adapters/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/";
+const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
+  : process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
 
 const api = axios.create({
   withCredentials: true,
