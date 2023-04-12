36371c2fd8823993318b08bdce5bd4f27f595fde
Mateus Toledo <mateusmtoledo1@gmail.com>
2023-04-11 21:11:46 -0300

    Fix URL issue

--- adapters/axios.ts
+++ adapters/axios.ts
@@ -1,8 +1,16 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
-  : process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api";
+let baseURL;
+
+if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
+  baseURL = "https://utube.mateustoledo.com/api";
+} else if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+  baseURL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`;
+} else if (process.env.NEXT_PUBLIC_API_URL) {
+  baseURL = process.env.NEXT_PUBLIC_API_URL;
+} else {
+  baseURL = "http://localhost:3000/api";
+}
 
 const api = axios.create({
   withCredentials: true,
