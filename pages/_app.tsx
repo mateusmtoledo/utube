286f19b357c45b998d94992eb820dfc95a6ab95f
Mateus Toledo <mateusmtoledo1@gmail.com>
2023-04-11 23:43:00 -0300

    Move Head tag to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import api from "@/adapters/axios";
 import { useState } from "react";
 import SidebarModalContext from "@/contexts/SidebarModalContext";
 import { SidebarModal } from "@/components/Sidebar";
+import Head from "next/head";
 
 export default function App({
   Component,
@@ -19,21 +20,28 @@ export default function App({
   }
 
   return (
-    <SWRConfig
-      value={{
-        fetcher: (url: string) => api.get(url).then((res) => res.data),
-      }}
-    >
-      <SessionProvider session={session}>
-        <SkeletonTheme baseColor="#334155" highlightColor="#475569">
-          <SidebarModalContext.Provider value={{ toggleSidebarModal }}>
-            <Component {...pageProps} />
-            {sidebarModalVisible && (
-              <SidebarModal toggleSidebarModal={toggleSidebarModal} />
-            )}
-          </SidebarModalContext.Provider>
-        </SkeletonTheme>
-      </SessionProvider>
-    </SWRConfig>
+    <>
+      <Head>
+        <meta name="description" content="UTube" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SWRConfig
+        value={{
+          fetcher: (url: string) => api.get(url).then((res) => res.data),
+        }}
+      >
+        <SessionProvider session={session}>
+          <SkeletonTheme baseColor="#334155" highlightColor="#475569">
+            <SidebarModalContext.Provider value={{ toggleSidebarModal }}>
+              <Component {...pageProps} />
+              {sidebarModalVisible && (
+                <SidebarModal toggleSidebarModal={toggleSidebarModal} />
+              )}
+            </SidebarModalContext.Provider>
+          </SkeletonTheme>
+        </SessionProvider>
+      </SWRConfig>
+    </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta name="description" content="UTube" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head />
       <body className="bg-slate-950 text-white">
         <Main />
         <div id="portal" />
